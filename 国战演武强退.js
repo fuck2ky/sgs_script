bf59842ab1617c8774c80cd0aa6714af2f386e3f
x <8650222667@qq.com>
2019-10-12 09:26:53 +0800

    优化

--- 国战演武强退.js
+++ 国战演武强退.js
@@ -1,3 +1,11 @@
+var modeId = 19; //19国战演武 20欢乐演武
+// 不在指定模式下先进入
+if (modeId != GameContext.GetModeType() && SceneManager.GetInstance().CurrentScene.sceneName != 'HallScene') {
+    ProtoProxy.GetInstance().SendProto(ProtoBufId.CMSG_CREQLOBBYJOIN, {
+        modeID: modeId
+    })
+}
+
 var startInterval = setInterval(function () {
     if (SceneManager.GetInstance().CurrentScene.sceneName == 'HallScene') {
         // 开始国战
@@ -11,10 +19,9 @@ var startInterval = setInterval(function () {
             "isForbiddenUseItem": false,
             "minEscapeRate": 0,
             "minJoinLevel": 0,
-            "modeID": 19,
+            "modeID": modeId,
             "passward": "sdfa",
-            "seatModelType": 3,
-            "tableName": "国战演武"
+            "seatModelType": 3
         })
         return;
     }
@@ -36,6 +43,23 @@ SelectCountryWarGeneralWindow.prototype.addEventListener = function () {
     SceneManager.GetInstance().CurrentScene.confirmQuitTbale();
 }
 
-var success = GameItemManager.GetInstance().GetItemByID(200010).itemNum;
-var fail = GameItemManager.GetInstance().GetItemByID(200011).itemNum;
-console.info("胜利箱子个数:"+success+" 失败箱子个数:"+fail);
+var formatDate = function (date) {  
+    var y = date.getFullYear();  
+    var m = date.getMonth() + 1;  
+    m = m < 10 ? ('0' + m) : m;  
+    var d = date.getDate();  
+    d = d < 10 ? ('0' + d) : d;  
+    var h = date.getHours();  
+    var minute = date.getMinutes();  
+    minute = minute < 10 ? ('0' + minute) : minute; 
+    var second= date.getSeconds();  
+    second = minute < 10 ? ('0' + second) : second;  
+    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+ second;  
+};
+function show() {
+    var success = GameItemManager.GetInstance().GetItemByID(200010).itemNum;
+    var fail = GameItemManager.GetInstance().GetItemByID(200011).itemNum;
+    console.info(formatDate(new Date()) + " 胜利箱子个数:" + success + " 失败箱子个数:" + fail);
+}
+show();
+var showInterval = setInterval(show, 60000);
